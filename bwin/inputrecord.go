7374bb4b0ff0af126b86b7f0bd1d4dd617dab5a6
Lenni <lennivh24@gmail.com>
2021-03-08 13:45:10 +0100

    fixed bwin

--- bwin/inputrecord.go
+++ bwin/inputrecord.go
@@ -2,7 +2,22 @@
 
 package bwin
 
+import "fmt"
+import "strings"
+
 type InputRecord struct {
 	Type uint16
 	Data [8]uint16
+}
+
+func (r *InputRecord) String() string {
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("Type: %d\n\r", r.Type))
+	sb.WriteString("Data: ")
+	for _, b := range r.Data {
+		sb.WriteString(fmt.Sprintf("0x%x ", b))
+	}
+	sb.WriteString("\n\r")
+
+	return sb.String()
 }
